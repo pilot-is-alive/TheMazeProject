414c6671e77f1891a64a3664c6ff1c247b519a32
Lusanda Nyilenda <lusandanyilenda@gmail.com>
2025-05-11 22:15:43 +0200

    Modify graph to add edges with weights

--- src/accse/datastructure/Graph.java
+++ src/accse/datastructure/Graph.java
@@ -1,6 +1,7 @@
 package accse.datastructure;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +9,7 @@ import java.util.Set;
 
 public class Graph<E> 
 {
-	public Map<E, List<E>> adjList;
+	public Map<E, Map<E, Double>> adjList;
 	
 	public Graph()
 	{
@@ -19,17 +20,17 @@ public class Graph<E>
 	{
 		if(!(adjList.containsKey(node)))
 		{
-			adjList.putIfAbsent(node, new ArrayList<>());
+			adjList.putIfAbsent(node, new HashMap<>());
 		}
 	}
 	
-	public void insertEdge(E from, E to)
+	public void insertEdge(E from, E to, double weight)
 	{
 		
 		if(adjList.containsKey(from) && adjList.containsKey(to))
 		{
-			adjList.get(from).add(to);
-			adjList.get(to).add(from);
+			adjList.get(from).put(to, weight);
+			adjList.get(to).put(from, weight);
 		}
 	}
 	
@@ -37,9 +38,15 @@ public class Graph<E>
 	{
 		if(adjList.containsKey(node))
 		{
-			for(E nodeNeighbour : adjList.get(node))
+			// copying the keyset to avoid ConcurrentModificationException 
+			// since we are modifying the underlying maps of the neighbours
+			Set<E> neighbours = Set.copyOf(adjList.get(node).keySet());
+			
+			for(E nodeNeighbour : neighbours)
 			{
-				adjList.get(nodeNeighbour).remove(node);
+				if (adjList.containsKey(nodeNeighbour)) {
+					adjList.get(nodeNeighbour).remove(node);
+				}
 			}
 			adjList.remove(node);
 		}
@@ -47,16 +54,45 @@ public class Graph<E>
 	
 	public void removeEdge(E from, E to)
 	{
-		if(adjList.containsKey(from) && adjList.containsKey(to))
+		if(adjList.containsKey(from) && adjList.get(from).containsKey(to))
 		{
 			adjList.get(from).remove(to);
+		}
+		if (adjList.containsKey(to) && adjList.get(to).containsKey(from)) {
 			adjList.get(to).remove(from);
 		}
 	}
 	
 	public List<E> getNodeNeighbours(E node)
 	{
-		return adjList.getOrDefault(node, new ArrayList<>());
+		if (adjList.containsKey(node)) {
+			return new ArrayList<>(adjList.get(node).keySet());
+		}
+		return new ArrayList<>();
+	}
+	
+	public Map<E, Double> getNodeNeighboursWeights(E node)
+	{
+		return adjList.getOrDefault(node, Collections.emptyMap());
+	}
+	
+	/**
+     * Gets the weight of the edge between two vertices.
+     *
+     * @param from The source vertex.
+     * @param to   The destination vertex.
+     * @return The weight of the edge, or null if the edge does not exist.
+     */
+	public Double getEdgeWeight(E from, E to) {
+		if (adjList.containsKey(from)) {
+			return adjList.get(from).get(to);
+		}
+		
+		return null;
+	}
+	
+	public boolean hasEdge(E from, E to) {
+		return adjList.containsKey(from) &&  adjList.get(from).containsKey(to);
 	}
 	
 	public Set<E> getVertices()
