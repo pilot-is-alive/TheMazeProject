0ceea2c776449e46366a0807fe86fd50d1b1f0bb
Lusanda Nyilenda <lusandanyilenda@gmail.com>
2025-05-13 01:20:12 +0200

    Apply Blessing's changes

--- src/ui/GuiDesign.java
+++ src/ui/GuiDesign.java
@@ -1,5 +1,8 @@
-package ProjectDesign;
+package ui;
 
+
+import java.io.File;
+import java.io.FileInputStream;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -11,22 +14,24 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import main.MyCanvas;
 
-public class GuiDesign extends Application {
+public class UserInterface extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		MyCanvas Canvas = new MyCanvas();
-		Canvas.setWidth(500);
-		Canvas.setHeight(500);
+		Canvas.setWidth(600);
+		Canvas.setHeight(550);
 		Canvas.prefWidth(500);
 		Canvas.prefHeight(500);
 		
@@ -35,6 +40,8 @@ public class GuiDesign extends Application {
 		canvasWrapper.setStyle("-fx-background-color: beige;");
 		
 		Button Wallbtn = new Button("Walls");
+		Wallbtn.setPrefWidth(200);
+		Wallbtn.setPrefHeight(40);
 		Image wallIcon = new Image(getClass().getResourceAsStream("/ProjectDesign/Resources/Wall.png"));
 		ImageView wallView = new ImageView(wallIcon);
 		wallView.setFitHeight(25);
@@ -42,6 +49,8 @@ public class GuiDesign extends Application {
 		Wallbtn.setGraphic(wallView);
 		
 		Button Invaderbtn = new Button("Invader");
+		Invaderbtn.setPrefWidth(200);
+		Invaderbtn.setPrefHeight(40);
 		Image invIcon = new Image(getClass().getResourceAsStream("/ProjectDesign/Resources/Invader.png"));
 		ImageView inView = new ImageView(invIcon);
 		inView.setFitHeight(30);
@@ -49,18 +58,13 @@ public class GuiDesign extends Application {
 		Invaderbtn.setGraphic(inView);
 		
 		Button Homeownerbtn = new Button("Homeowner");
+		Homeownerbtn.setPrefWidth(200);
+		Homeownerbtn.setPrefHeight(40);
 		Image homeIcon = new Image(getClass().getResourceAsStream("/ProjectDesign/Resources/HomeOwner.png")); 
 		ImageView homeView = new ImageView(homeIcon);
 		homeView.setFitHeight(30);
 		homeView.setFitWidth(30);
 		Homeownerbtn.setGraphic(homeView);
-		
-		Button Undobtn = new Button("Undo");
-		Image undoIcon = new Image(getClass().getResourceAsStream("/ProjectDesign/Resources/undo.png"));
-		ImageView undoView = new ImageView(undoIcon);
-		undoView.setFitHeight(20);
-		undoView.setFitWidth(20);
-		Undobtn.setGraphic(undoView);
 		
 		Button exitbtn = new Button("Exit");
 		
@@ -71,11 +75,51 @@ public class GuiDesign extends Application {
 		Startbtn.setStyle("-fx-background-color: Lightgreen; -fx-text-fill: white;");
 		
 		Button resetBtn = new Button("Reset");
+		resetBtn.setOnAction(e->{
+			Canvas.clearCanvas();
+			
+		});
 	    Button saveBtn = new Button("Save Map");
+	    
 	    Button loadBtn = new Button("Load Map");
+	    
+	    loadBtn.setOnAction(e -> {
+	        FileChooser fileChooser = new FileChooser();
+	        fileChooser.setTitle("Open Blueprint Image");
+	        fileChooser.getExtensionFilters().addAll(
+	            new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+	        );
+
+	        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+
+	        if (selectedFile != null) {
+	            try {
+	                FileInputStream input = new FileInputStream(selectedFile);
+	                Image blueprintImage = new Image(input);
+
+	                // Draw image on canvas
+	                Canvas.setBackgroundImage(blueprintImage);
+	            } catch (Exception ex) {
+	                ex.printStackTrace();
+	            }
+	        }
+	    });
+
+	    
+	    Button Undobtn = new Button("Undo");
+		Undobtn.setPrefWidth(200);
+		Undobtn.setPrefHeight(40);
+		Undobtn.setStyle("-fx-background-color: Grey; -fx-text-fill: Black;");
+		Image undoIcon = new Image(getClass().getResourceAsStream("/ProjectDesign/Resources/undo.png"));
+		ImageView undoView = new ImageView(undoIcon);
+		undoView.setFitHeight(20);
+		undoView.setFitWidth(20);
+		Undobtn.setGraphic(undoView);
 	    
 	    TextArea logArea = new TextArea();
         logArea.setText("Logs, test scores, and error messages...\n");
+        logArea.setPrefWidth(10);
+        logArea.setPrefHeight(200);
         logArea.setEditable(true);
 		
 		Undobtn.setOnAction(e -> {
@@ -84,39 +128,42 @@ public class GuiDesign extends Application {
         });
 		
 	
-		VBox leftPanel = new VBox(10, Wallbtn, Invaderbtn, Homeownerbtn, Undobtn);
-        leftPanel.setAlignment(Pos.TOP_CENTER);
-        leftPanel.setPadding(new Insets(20,10,20,10));
+		HBox Bottompanel = new HBox(10, Wallbtn, Invaderbtn, Homeownerbtn, Undobtn);
+        Bottompanel.setAlignment(Pos.CENTER);
+        Bottompanel.setPadding(new Insets(0));
 
-        Region spacer = new Region();
-        spacer.setPrefHeight(200);
+        Region srtspacer = new Region();
+        srtspacer.setPrefHeight(150);
+        
+        Region logspacer= new Region();
+        logspacer.setPrefHeight(50);
 
-        VBox rightPanel = new VBox(10, resetBtn, saveBtn, loadBtn, spacer, Startbtn);
+        VBox rightPanel = new VBox(10, resetBtn, saveBtn, loadBtn,logspacer, logArea ,srtspacer, Startbtn);
         rightPanel.setAlignment(Pos.TOP_CENTER);
-        rightPanel.setPadding(new Insets(20,10,20,10));
+        rightPanel.setPadding(new Insets(0));
         
-        GridPane rightPane = new GridPane();
-        rightPane.setPrefWidth(150);
-        rightPane.setPrefHeight(150);
-        rightPane.setPadding(new Insets(10));
-        rightPane.add(rightPanel, 0, 0);
-	     
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10));
-        grid.setHgap(10);
-        grid.setVgap(10);
-        GridPane.setFillWidth(rightPane, false);
-        GridPane.setFillHeight(rightPane, false);
-        GridPane.setHalignment(rightPane, javafx.geometry.HPos.CENTER);
-        GridPane.setValignment(rightPane, javafx.geometry.VPos.CENTER);
+		
+		 GridPane rightPane = new GridPane(); 
+		 rightPane.setPrefWidth(150);
+		 rightPane.setPrefHeight(150); rightPane.setPadding(new Insets(10));
+		 rightPane.add(rightPanel, 0, 0);
+		  
+		  GridPane grid = new GridPane();
+		  grid.setPadding(new Insets(10));
+		  grid.setHgap(10);
+		  grid.setVgap(10);
+		  GridPane.setFillWidth(rightPane, false);
+		  GridPane.setFillHeight(rightPane, false);
+		  GridPane.setHalignment(rightPane,javafx.geometry.HPos.CENTER); GridPane.setValignment(rightPane, javafx.geometry.VPos.CENTER);
+		 
         
-        grid.add(leftPanel, 0, 0);
+       // grid.add(leftPanel, 0, 0);
 
 		 BorderPane root = new BorderPane();
 		 root.setLeft(grid);
-		 root.setRight(rightPane); 
+		 root.setRight(rightPanel); 
 		 root.setCenter(canvasWrapper);
-		 root.setBottom(logArea);
+		 root.setBottom(Bottompanel);
 		 root.setStyle("-fx-background-color: lightblue;");
 		
 		 Scene scene = new Scene(root, 800, 650);
