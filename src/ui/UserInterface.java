f78256e02de766ca0bb255ded48d3d83ccd020a4
Lusanda Nyilenda <lusandanyilenda@gmail.com>
2025-05-17 20:29:42 +0200

    Set correct indentation for UserInterface code

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -27,7 +27,7 @@ import main.GraphConverter;
 import main.MyCanvas;
 
 public class UserInterface extends Application {
-	
+
 	private GraphConverter converter = null;
 
 	@Override
@@ -37,11 +37,11 @@ public class UserInterface extends Application {
 		Canvas.setHeight(550);
 		Canvas.prefWidth(500);
 		Canvas.prefHeight(500);
-		
+
 		StackPane canvasWrapper = new StackPane(Canvas);
 		canvasWrapper.setPrefSize(500, 500);
 		canvasWrapper.setStyle("-fx-background-color: beige;");
-		
+
 		Button Wallbtn = new Button("Walls");
 		Wallbtn.setPrefWidth(200);
 		Wallbtn.setPrefHeight(40);
@@ -53,7 +53,7 @@ public class UserInterface extends Application {
 		Wallbtn.setOnAction(e -> {
 			Canvas.drawChoice(DrawChoice.WALL);
 		});
-		
+
 		Button Invaderbtn = new Button("Invader");
 		Invaderbtn.setPrefWidth(200);
 		Invaderbtn.setPrefHeight(40);
@@ -65,7 +65,7 @@ public class UserInterface extends Application {
 		Invaderbtn.setOnAction(e -> {
 			Canvas.drawChoice(DrawChoice.INTRUDER);
 		});
-		
+
 		Button Homeownerbtn = new Button("Homeowner");
 		Homeownerbtn.setPrefWidth(200);
 		Homeownerbtn.setPrefHeight(40);
@@ -77,9 +77,9 @@ public class UserInterface extends Application {
 		Homeownerbtn.setOnAction(e -> {
 			Canvas.drawChoice(DrawChoice.ESCAPER);
 		});
-		
+
 		Button exitbtn = new Button("Exit");
-		
+
 		Button Startbtn = new Button("Start");
 		Startbtn.setPrefWidth(120);
 		Startbtn.setPrefHeight(40);
@@ -88,42 +88,42 @@ public class UserInterface extends Application {
 		Startbtn.setOnAction(e -> {
 			converter =  new GraphConverter();
 			converter.createGraphFromCanvas(Canvas);
-			
+
 		});
-		
+
 		Button resetBtn = new Button("Reset");
 		resetBtn.setOnAction(e->{
 			Canvas.reset();
-			
+
 		});
-	    Button saveBtn = new Button("Save Map");
-	    
-	    Button loadBtn = new Button("Load Map");
-	    
-	    loadBtn.setOnAction(e -> {
-	        FileChooser fileChooser = new FileChooser();
-	        fileChooser.setTitle("Open Blueprint Image");
-	        fileChooser.getExtensionFilters().addAll(
-	            new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
-	        );
+		Button saveBtn = new Button("Save Map");
 
-	        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+		Button loadBtn = new Button("Load Map");
 
-	        if (selectedFile != null) {
-	            try {
-	                FileInputStream input = new FileInputStream(selectedFile);
-	                Image blueprintImage = new Image(input);
+		loadBtn.setOnAction(e -> {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Open Blueprint Image");
+			fileChooser.getExtensionFilters().addAll(
+					new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+					);
 
-	                // Draw image on canvas
-	                Canvas.setBackgroundImage(blueprintImage);
-	            } catch (Exception ex) {
-	                ex.printStackTrace();
-	            }
-	        }
-	    });
+			File selectedFile = fileChooser.showOpenDialog(primaryStage);
 
-	    
-	    Button Undobtn = new Button("Undo");
+			if (selectedFile != null) {
+				try {
+					FileInputStream input = new FileInputStream(selectedFile);
+					Image blueprintImage = new Image(input);
+
+					// Draw image on canvas
+					Canvas.setBackgroundImage(blueprintImage);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+
+
+		Button Undobtn = new Button("Undo");
 		Undobtn.setPrefWidth(200);
 		Undobtn.setPrefHeight(40);
 		Undobtn.setStyle("-fx-background-color: Grey; -fx-text-fill: Black;");
@@ -132,63 +132,63 @@ public class UserInterface extends Application {
 		undoView.setFitHeight(20);
 		undoView.setFitWidth(20);
 		Undobtn.setGraphic(undoView);
-	    
-	    TextArea logArea = new TextArea();
-        logArea.setText("Logs, test scores, and error messages...\n");
-        logArea.setPrefWidth(10);
-        logArea.setPrefHeight(200);
-        logArea.setEditable(true);
-		
+
+		TextArea logArea = new TextArea();
+		logArea.setText("Logs, test scores, and error messages...\n");
+		logArea.setPrefWidth(10);
+		logArea.setPrefHeight(200);
+		logArea.setEditable(true);
+
 		Undobtn.setOnAction(e -> {
-            Canvas.undo();
-            logArea.appendText("Undo last line.\n");
-        });
-		
-	
+			Canvas.undo();
+			logArea.appendText("Undo last line.\n");
+		});
+
+
 		HBox Bottompanel = new HBox(10, Wallbtn, Invaderbtn, Homeownerbtn, Undobtn);
-        Bottompanel.setAlignment(Pos.CENTER);
-        Bottompanel.setPadding(new Insets(0));
+		Bottompanel.setAlignment(Pos.CENTER);
+		Bottompanel.setPadding(new Insets(0));
 
-        Region srtspacer = new Region();
-        srtspacer.setPrefHeight(150);
-        
-        Region logspacer= new Region();
-        logspacer.setPrefHeight(50);
+		Region srtspacer = new Region();
+		srtspacer.setPrefHeight(150);
 
-        VBox rightPanel = new VBox(10, resetBtn, saveBtn, loadBtn,logspacer, logArea ,srtspacer, Startbtn);
-        rightPanel.setAlignment(Pos.TOP_CENTER);
-        rightPanel.setPadding(new Insets(0));
-        
-		
-		 GridPane rightPane = new GridPane(); 
-		 rightPane.setPrefWidth(150);
-		 rightPane.setPrefHeight(150); rightPane.setPadding(new Insets(10));
-		 rightPane.add(rightPanel, 0, 0);
-		  
-		  GridPane grid = new GridPane();
-		  grid.setPadding(new Insets(10));
-		  grid.setHgap(10);
-		  grid.setVgap(10);
-		  GridPane.setFillWidth(rightPane, false);
-		  GridPane.setFillHeight(rightPane, false);
-		  GridPane.setHalignment(rightPane,javafx.geometry.HPos.CENTER); GridPane.setValignment(rightPane, javafx.geometry.VPos.CENTER);
-		 
-        
-       // grid.add(leftPanel, 0, 0);
+		Region logspacer= new Region();
+		logspacer.setPrefHeight(50);
 
-		 BorderPane root = new BorderPane();
-		 root.setLeft(grid);
-		 root.setRight(rightPanel); 
-		 root.setCenter(canvasWrapper);
-		 root.setBottom(Bottompanel);
-		 root.setStyle("-fx-background-color: lightblue;");
-		
-		 Scene scene = new Scene(root, 800, 650);
-		 primaryStage.setTitle("Home Invader Simulator ");
-		 primaryStage.setScene(scene);
-		 primaryStage.show();
-		 Canvas.render();
+		VBox rightPanel = new VBox(10, resetBtn, saveBtn, loadBtn,logspacer, logArea ,srtspacer, Startbtn);
+		rightPanel.setAlignment(Pos.TOP_CENTER);
+		rightPanel.setPadding(new Insets(0));
+
+
+		GridPane rightPane = new GridPane(); 
+		rightPane.setPrefWidth(150);
+		rightPane.setPrefHeight(150); rightPane.setPadding(new Insets(10));
+		rightPane.add(rightPanel, 0, 0);
+
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10));
+		grid.setHgap(10);
+		grid.setVgap(10);
+		GridPane.setFillWidth(rightPane, false);
+		GridPane.setFillHeight(rightPane, false);
+		GridPane.setHalignment(rightPane,javafx.geometry.HPos.CENTER); GridPane.setValignment(rightPane, javafx.geometry.VPos.CENTER);
+
+
+		// grid.add(leftPanel, 0, 0);
+
+		BorderPane root = new BorderPane();
+		root.setLeft(grid);
+		root.setRight(rightPanel); 
+		root.setCenter(canvasWrapper);
+		root.setBottom(Bottompanel);
+		root.setStyle("-fx-background-color: lightblue;");
+
+		Scene scene = new Scene(root, 800, 650);
+		primaryStage.setTitle("Home Invader Simulator ");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		Canvas.render();
 	}
-	
+
 
 }
